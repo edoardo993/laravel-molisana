29fcecb22ddf59d4c5f5040ff1d70834406711d2
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-11 18:06:41 +0100

    layout.blade.php added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -137,19 +137,8 @@ $data = '[
 
   @endphp
 
-  <header id="header">
-
-    <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="" class="logo">
-    
-    <ul class="header-list">
-      <li>Home</li>
-      <li>Prodotti</li>
-      <li>News</li>
-    </ul>
-
-  </header>
-
-  <main class="main-content">
+  @extends('layout.layout')
+  @section('content')
 
     <div class="boxes">
 
@@ -159,11 +148,12 @@ $data = '[
 
         <div class="pasta-type">
 
-          @foreach ($array as $key => $pasta)
+          @foreach ($array as $id => $pasta)
             @if($pasta['tipo'] === 'lunga')
-              <img src="{{$pasta['src']}}" alt="">
+              <a href="/product{{$id}}"><img src="{{$pasta['src']}}" alt=""></a>
             @endif
           @endforeach
+
         </div>
 
       </div>
@@ -199,77 +189,7 @@ $data = '[
       </div>
 
     </div>
-
-  </main>
-
-  <footer id="footer">
-
-    <div class="footer-content">
-
-      <div class="la-molisana-info">
-
-        <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="" class="logo">
-        <ul>
-          <li>Ragione sociale: La Molisana S.P.A.</li>
-          <li>Sede legale: Contrada Colle delle Alpi, 100/A</li>
-          <li>86100 - Campobasso (CB)</li>
-          <li>Pec: lamolisana@pec.it</li>
-          <li>Tel: +39 0874 4981</li>
-          <li>Fax: +39 0874 629584</li>
-          <li>info@lamolisana.it (per segnalazioni degli utenti)</li>
-          <li>commerciale@lamolisana.it</li>
-          <li>export@lamolisana.it</li>
-          <li>numero verde 800818081</li>
-          <li>telefono 3801292455</li>
-        </ul>
-
-      </div>
-
-      <div class="la-molisana-info">
-
-        <h4 class="footer-title">pastificio</h4>
-
-        <ul>
-          <li>Il Pastificio</li>
-          <li>Grano decorticato a pietra</li>
-          <li>Il Molise c'è</li>
-          <li>Filiera Integrata</li>
-          <li>100 anni di pasta</li>
-          <li>Sartoria della pasta</li>
-          <li>Spaghetto Quadrato</li>
-          <li>Le Persone</li>
-        </ul>
-
-        <h4 class="footer-title">prodotti</h4>
-
-        <ul>
-          <li>Le Classiche</li>
-          <li>Le Integrali</li>
-          <li>Le Speciali</li>
-          <li>Le Biologiche</li>
-          <li>Le Gluten-Free</li>
-          <li>Le Semole</li>
-          <li>Le Extra di Lusso</li>
-        </ul>
-
-      </div>
-
-      <div class="la-molisana-info">
-
-        <h4 class="footer-title">collezione da chef</h4>
-
-        <ul>
-          <li>Collezione da chef</li>
-          <li>Grandi Cucine</li>
-          <li>Biologiche</li>
-          <li>Quadrate</li>
-        </ul>
-
-      </div>
-
-    </div>
-
-  </footer>
+  @endsection
 
 </body>
 </html>
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -11,15 +11,88 @@
 
         <header id="header">
 
-        </header>
+            <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="" class="logo">
+            
+            <ul class="header-list">
+              <li>Home</li>
+              <li>Prodotti</li>
+              <li>News</li>
+            </ul>
+        
+          </header>
 
-        <main>
+        <main class="main-content">
             @yield('content')
         </main>
 
         <footer id="footer">
 
-        </footer>
+            <div class="footer-content">
+        
+              <div class="la-molisana-info">
+        
+                <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="" class="logo">
+                <ul>
+                  <li>Ragione sociale: La Molisana S.P.A.</li>
+                  <li>Sede legale: Contrada Colle delle Alpi, 100/A</li>
+                  <li>86100 - Campobasso (CB)</li>
+                  <li>Pec: lamolisana@pec.it</li>
+                  <li>Tel: +39 0874 4981</li>
+                  <li>Fax: +39 0874 629584</li>
+                  <li>info@lamolisana.it (per segnalazioni degli utenti)</li>
+                  <li>commerciale@lamolisana.it</li>
+                  <li>export@lamolisana.it</li>
+                  <li>numero verde 800818081</li>
+                  <li>telefono 3801292455</li>
+                </ul>
+        
+              </div>
+        
+              <div class="la-molisana-info">
+        
+                <h4 class="footer-title">pastificio</h4>
+        
+                <ul>
+                  <li>Il Pastificio</li>
+                  <li>Grano decorticato a pietra</li>
+                  <li>Il Molise c'è</li>
+                  <li>Filiera Integrata</li>
+                  <li>100 anni di pasta</li>
+                  <li>Sartoria della pasta</li>
+                  <li>Spaghetto Quadrato</li>
+                  <li>Le Persone</li>
+                </ul>
+        
+                <h4 class="footer-title">prodotti</h4>
+        
+                <ul>
+                  <li>Le Classiche</li>
+                  <li>Le Integrali</li>
+                  <li>Le Speciali</li>
+                  <li>Le Biologiche</li>
+                  <li>Le Gluten-Free</li>
+                  <li>Le Semole</li>
+                  <li>Le Extra di Lusso</li>
+                </ul>
+        
+              </div>
+        
+              <div class="la-molisana-info">
+        
+                <h4 class="footer-title">collezione da chef</h4>
+        
+                <ul>
+                  <li>Collezione da chef</li>
+                  <li>Grandi Cucine</li>
+                  <li>Biologiche</li>
+                  <li>Quadrate</li>
+                </ul>
+        
+              </div>
+        
+            </div>
+        
+          </footer>
 
     </body>
 </html>
