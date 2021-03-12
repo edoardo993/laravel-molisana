91256fded6b662684bd37e81168f6713dcaa48d4
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-12 15:36:50 +0100

    slider arrows created

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -5,8 +5,8 @@
     <div class="product-info">
         <h5 class="product-title">{{$pastaArray[$idProduct]['titolo']}}</h5>
         <img src="{{$pastaArray[$idProduct]['src-h']}}" alt="">
-        <i class="fas fa-chevron-left left-arrow"></i>
-        <i class="fas fa-chevron-right right-arrow"></i>
+        <div class="left-slider-arrow"><i class="fas fa-chevron-left left-arrow"></i></div>
+        <div class="right-slider-arrow"><i class="fas fa-chevron-right right-arrow"></i></div>
         <img src="{{$pastaArray[$idProduct]['src-p']}}" alt="">
         <p class="product-description">{!!$pastaArray[$idProduct]['descrizione']!!}</p>
     </div>
