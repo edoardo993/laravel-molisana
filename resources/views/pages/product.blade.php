b8074bfd06afa4acd997b7a9178fbc9b4671d47d
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-12 17:21:27 +0100

    slider

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -3,12 +3,24 @@
   <main class="main-product-content">
 
     <div class="product-info">
+
         <h5 class="product-title">{{$pastaArray[$idProduct]['titolo']}}</h5>
         <img src="{{$pastaArray[$idProduct]['src-h']}}" alt="">
-        <div class="left-slider-arrow"><i class="fas fa-chevron-left left-arrow"></i></div>
-        <div class="right-slider-arrow"><i class="fas fa-chevron-right right-arrow"></i></div>
         <img src="{{$pastaArray[$idProduct]['src-p']}}" alt="">
         <p class="product-description">{!!$pastaArray[$idProduct]['descrizione']!!}</p>
+
+        @if($idProduct === 0)
+            <a href="/product/{{count($pastaArray)}}"><div class="left-slider-arrow"><i class="fas fa-chevron-left left-arrow"></i></div></a>
+        @else
+            <a href="/product/{{$idProduct - 1}}"><div class="left-slider-arrow"><i class="fas fa-chevron-left left-arrow"></i></div></a>
+        @endif
+
+        @if($idProduct === count($pastaArray))
+            <a href="/product/0"><div class="right-slider-arrow"><i class="fas fa-chevron-right right-arrow"></i></div></a>
+        @else
+            <a href="/product/{{$idProduct + 1}}"><div class="right-slider-arrow"><i class="fas fa-chevron-right right-arrow"></i></div></a>
+        @endif
+        
     </div>
 
   </main>
