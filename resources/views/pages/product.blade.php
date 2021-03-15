4c6e54d1291ffc1874bcd7c91a76a551370d1b54
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-03-15 16:41:40 +0100

    updates

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -9,13 +9,13 @@
         <img src="{{$pastaArray[$idProduct]['src-p']}}" alt="">
         <p class="product-description">{!!$pastaArray[$idProduct]['descrizione']!!}</p>
 
-        @if($idProduct === 0)
-            <a href="/product/{{count($pastaArray)}}"><div class="left-slider-arrow"><i class="fas fa-chevron-left left-arrow"></i></div></a>
+        @if($idProduct == 0)
+            <a href="/product/{{count($pastaArray) - 1}}"><div class="left-slider-arrow"><i class="fas fa-chevron-left left-arrow"></i></div></a>
         @else
             <a href="/product/{{$idProduct - 1}}"><div class="left-slider-arrow"><i class="fas fa-chevron-left left-arrow"></i></div></a>
         @endif
 
-        @if($idProduct === count($pastaArray))
+        @if($idProduct == count($pastaArray) - 1)
             <a href="/product/0"><div class="right-slider-arrow"><i class="fas fa-chevron-right right-arrow"></i></div></a>
         @else
             <a href="/product/{{$idProduct + 1}}"><div class="right-slider-arrow"><i class="fas fa-chevron-right right-arrow"></i></div></a>
